594021acdbe291150883fdc4933a86472534154d
Denise Baker <denviol@yahoo.com>
2019-10-25 16:07:35 -0500

    set up data array and helpers component

---
+++ src/javascripts/helpers/data.js
@@ -0,0 +1,15 @@
+const messages = [
+  {
+    id: 'user1',
+    message: 'OMG! Hi!!!!',
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Avatar_TRYPKO.jpg',
+  },
+  {
+    id: 'user2',
+    message: 'Like, I was literally just thinking about texting you!',
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQcKVrFa5_weBOxqvU6S45sRG17e18OOo37-Am7eaPF1OvZ8Gkt',
+  }];
+
+const getMessages = () => messages;
+
+export default { getMessages };
