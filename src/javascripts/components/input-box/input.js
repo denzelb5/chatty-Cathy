10b9e975fec7b258edf8bfbc213590a9f57efee5
Denise Baker <denviol@yahoo.com>
2019-10-28 16:25:34 -0500

    added 2nd user function

--- src/javascripts/components/input-box/input.js
+++ src/javascripts/components/input-box/input.js
@@ -7,8 +7,15 @@ const printNewMessage = () => {
     const newMessage = $('#input-box').val().toLowerCase();
     const messageArray = d.getMessages();
     const newData = {};
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13 && $('#check1').is(':checked')) {
+      newData.avatar = 'https://upload.wikimedia.org/wikipedia/commons/8/88/Avatar_TRYPKO.jpg';
       newData.message = newMessage;
+      newData.id = $('#user1');
+      messageArray.push(newData);
+    } else if (e.keyCode === 13 && $('#check2').is(':checked')) {
+      newData.avatar = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQcKVrFa5_weBOxqvU6S45sRG17e18OOo37-Am7eaPF1OvZ8Gkt';
+      newData.message = newMessage;
+      newData.id = $('#user2');
       messageArray.push(newData);
     }
     m.printMessages(messageArray);
